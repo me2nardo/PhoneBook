36c6b05e7b03c8d46e89ffe14ff0e1baedb21906
vitalii.levash <mr.roooot@gmail.com>
2016-04-19 12:29:17 +0300

    Task: Fix Sec Service

--- src/main/java/com/lardi/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/lardi/service/impl/SecurityServiceImpl.java
@@ -3,10 +3,12 @@ package com.lardi.service.impl;
 import com.lardi.model.User;
 import com.lardi.service.SecurityService;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by leo on 18.04.2016.
  */
+@Service
 public class SecurityServiceImpl implements SecurityService {
 
     @Override
